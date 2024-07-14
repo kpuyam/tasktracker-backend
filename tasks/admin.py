930558b65a8f825e38ed6d1b52749df125c33a51
Kelvin <162977927+kpuyam@users.noreply.github.com>
2024-07-14 12:53:43 +0530

    Update admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,18 +2,3 @@ from django.contrib import admin
 from .models import Project, Task, Role
 
 # Register your models here.
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'start_date', 'end_date', 'owner')
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('description', 'due_date', 'status', 'project', 'owner')
-    list_filter = ('status', 'project')
-    search_fields = ('description',)
-
-@admin.register(Role)
-class RoleAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    filter_horizontal = ('users',)
