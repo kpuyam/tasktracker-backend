707b5aa7b4656dacea3e7ff77fe4d0cf877fa3b3
Pathakottu Reddy <pathakottu.reddy@tigeranalytics.local>
2024-07-18 18:25:20 +0530

    Updated django for profile building and credentials

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -18,6 +18,19 @@ class RoleSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            email=validated_data.get('email', '')
+        )
+        return user
--- tasktracker/urls.py
+++ tasktracker/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from tasks.views import find_projects, ProjectViewSet, TaskViewSet, RoleViewSet, SignupView, UserViewSet
+from tasks.views import find_projects,get_user_details, ProjectViewSet, TaskViewSet, RoleViewSet, SignupView, UserViewSet
 
 router = DefaultRouter()
 router.register(r"projects", ProjectViewSet)
@@ -32,6 +32,8 @@ urlpatterns = [
     path("api/", include(router.urls)),
     path("api/findProjects/", find_projects),
     path('api/signup/', SignupView.as_view(), name='signup'),
+    path('api/user_details/', get_user_details, name='user_details'),
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
