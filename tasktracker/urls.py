0f1365f5d1fe7365713a195a2ba9a2278de3ad08
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-07-18 11:45:47 +0530

    Update Urls

--- tasktracker/urls.py
+++ tasktracker/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from tasks.views import find_projects, ProjectViewSet, TaskViewSet, RoleViewSet
+from tasks.views import find_projects, ProjectViewSet, TaskViewSet, RoleViewSet, SignupView
 
 router = DefaultRouter()
 router.register(r"projects", ProjectViewSet)
@@ -30,6 +30,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api/findProjects/", find_projects),
+    path('api/signup/', SignupView.as_view(), name='signup'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
