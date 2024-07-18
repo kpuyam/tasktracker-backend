31530a56dcf8c7d0cf914a2d320e964561e52243
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-07-18 13:31:29 +0530

    Update Serializers

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Project, Task, Role
 
@@ -15,3 +16,8 @@ class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
