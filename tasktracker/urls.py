ffd70f55f9a599c596bb7343ad4beaea2a87c260
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-07-19 11:20:02 +0530

    Update Urls

--- tasktracker/urls.py
+++ tasktracker/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path("api/findProjects/", find_projects),
     path('api/signup/', SignupView.as_view(), name='signup'),
     path('api/user_details/', get_user_details, name='user_details'),
-
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
