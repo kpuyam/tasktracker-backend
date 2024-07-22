8c9b0512fd2b2913fb43d6c492a4a16607f54c93
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-07-22 14:39:03 +0530

    Update Models

--- tasks/models.py
+++ tasks/models.py
@@ -40,8 +40,8 @@ class Role(models.Model):
 
     name = models.CharField(max_length=50, choices=ROLE_CHOICES, unique=True)
     users = models.ManyToManyField(User, related_name='roles')
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='roles')
+    # project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='roles')
     
-    def __str__(self):
-        users = ", ".join([user.username for user in self.users.all()])
-        return f"{users} - {self.get_name_display()} in {self.project.name}"
+    # def __str__(self):
+    #     users = ", ".join([user.username for user in self.users.all()])
+    #     return f"{users} - {self.get_name_display()} in {self.project.name}"
